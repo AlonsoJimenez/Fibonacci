cfd29f6cc53aa27f2e984d84b3f0e4274a19fbb6
Alonso <alonso.jimenez@hotmail.com>
2020-07-28 22:47:56 -0600

    Correccion render template

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,11 +1,12 @@
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def launch():
-    return "Prueba"
+    return render_template('calculator.html')
 
 
 if __name__ == '__main__':
