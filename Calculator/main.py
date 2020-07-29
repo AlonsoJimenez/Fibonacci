a29076be62858d7174dad855af30c9fe40861932
Alonso <alonso.jimenez@hotmail.com>
2020-07-28 22:44:02 -0600

    Prueba commit

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,10 +1,11 @@
 
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def launch():
-    return app.send_static_file('calculator.html')
+    return "Prueba"
 
 
 if __name__ == '__main__':
